016a0eaad83cbc7370615d0176975f84e8d28ade
DhatriJangam <dhatrijangam602@gmail.com>
2024-07-23 21:06:28 +0530

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,34 @@
+public class Movie {
+    private String title;
+    private String director;
+    private int year;
+    private double rating;
+
+    public Movie(String title, String director, int year, double rating) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Title: %s, Director: %s, Year: %d, Rating: %.1f", title, director, year, rating);
+    }
+}
